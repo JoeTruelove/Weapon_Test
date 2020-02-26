698295128f607373bdf790a0ee8e643624184828
Aidan Varkoly <47114022+AidanVarkoly@users.noreply.github.com>
2020-02-25 19:19:37 -0500

    Elevator

---
+++ Assets/Scripts/DoorAnim.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnim : MonoBehaviour
+{
+
+    public Animator animator;
+    public LayerMask Player;
+
+    void Start()
+    {
+        Player = GetComponent<LayerMask>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            animator.SetBool("Door Open", true);
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            StartCoroutine("ExitTimer");
+        }
+    }
+
+
+    IEnumerator ExitTimer()
+    {
+        yield return new WaitForSeconds(2);
+        animator.SetBool("Door Open", false);
+    }
+}
+
